6971a72024ed42c247d7452feccccaae14ed976c
unknown <ghekghek2@gmail.com>
2023-02-24 01:56:50 +0800

    add email features

--- connect.js
+++ connect.js
@@ -1,8 +1,11 @@
+
+      
 function myF(item) {
   
 
     if(item.value === ""){
         item.nextElementSibling.classList.add("up")
+        item.classList.add("inputAct")
     }
 
 
@@ -10,13 +13,26 @@ function myF(item) {
 
         if(item.value == ""){
             item.nextElementSibling.classList.remove("up")
+            item.classList.remove("inputAct")
         }
       
        
       });
 
+
+
+
+
   }
 
 
 
-      
+
+
+window.addEventListener("load", (event) => {
+    const input = document.querySelectorAll(".textBox")
+
+    input.forEach(tbox => tbox.value = '')
+ 
+  
+  });
