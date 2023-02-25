71be271c30d08b14f08b62546d0421a63fa8b4df
unknown <ghekghek2@gmail.com>
2023-02-25 18:00:04 +0800

    add footer

--- nav.js
+++ nav.js
@@ -83,3 +83,13 @@ window.onscroll = () => {
 
 
 };
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      },2000);
+  });
+});
